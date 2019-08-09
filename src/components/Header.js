1b2f301e3b90ffc9cad410a3a57cf5b8ab0c1830
elmaximi <elmaximi26@gmail.com>
2019-08-09 16:29:07 -0500

    creacion de barra de navegacion

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 const Header =() =>(
     <nav className ="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
             <Link to ="/productos" className="navbar-brand">
                 React CRUD y Routing
                 </Link>
+            <ul className="navbar-nav mr-auto"> 
+                <li className="nav-item">
+                   <NavLink
+                    to='/productos'
+                    className="nav-link"
+                    activeClassName="active"
+                   >Productos</NavLink> 
+                </li>
+
+                <li className="nav-item">
+                   <NavLink
+                    to='/nuevo-producto'
+                    className="nav-link"
+                    activeClassName="active"
+                   >Nuevo Producto</NavLink> 
+                </li>
+
+            </ul>
         </div>
     </nav>
 );
