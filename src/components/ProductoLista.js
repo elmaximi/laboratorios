fe938d79339a6d8a57dea254db81ff4f2561292b
elmaximi <elmaximi26@gmail.com>
2019-08-10 12:05:56 -0500

    mostramos los datos de la API

--- src/components/ProductoLista.js
+++ src/components/ProductoLista.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function ProductoLista(){
+function ProductoLista({producto}){
     return(
-        <div></div>
+        <li data-categoria={producto.categoria} className="list-group-item d-flex justify-content-between 
+        alig-items-center">
+            <p>
+                {producto.nombrePlatillo} {'  '}
+                <span className="font-weight-blod">${producto.precioPlatillo}</span>
+            </p>
+            <div>
+
+            </div>
+        </li>
     )
 }
 
